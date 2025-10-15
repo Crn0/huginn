b06d31ce3611a18e3053c9647de8017ada47e941
crno <118383162+Crn0@users.noreply.github.com.>
2025-10-15 10:44:40 +0800

    refactor(ui): simplify AuthLayout and remove pathname-based title

--- src/components/layouts/auth-layout.tsx
+++ src/components/layouts/auth-layout.tsx
@@ -1,27 +1,16 @@
 import type { ReactNode } from "react";
-import { useLocation } from "@tanstack/react-router";
 import { Origami } from "lucide-react";
 
 export function AuthLayout({ children }: { children: ReactNode }) {
-  const location = useLocation();
-
-  const pathname = location.pathname;
-
-  const title = `${pathname.slice(1, 2).toUpperCase()}${pathname.slice(2)}`;
-
   return (
-    <div className='flex min-h-dvh flex-col items-stretch gap-5 bg-black text-white sm:flex sm:min-h-dvh sm:flex-col sm:gap-10 sm:p-5'>
+    <div className='flex min-h-dvh flex-col items-stretch gap-5 bg-neutral-800 text-white sm:flex sm:min-h-dvh sm:flex-col sm:gap-10 sm:p-5'>
       <div className='flex items-center gap-5 self-center sm:self-auto'>
         <div className='flex justify-center'>
           <Origami className='h-24 w-auto' />
         </div>
-
-        <h1>{title}</h1>
       </div>
 
-      <div className='sm:grid sm:h-auto sm:place-content-center sm:place-items-center sm:gap-5 sm:self-center'>
-        {children}
-      </div>
+      <div className='w-sm self-center-safe'>{children}</div>
     </div>
   );
 }
