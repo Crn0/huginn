1c3707b5a45cc805a3e3f6f7a76a02f1d61f8928
crno <118383162+Crn0@users.noreply.github.com.>
2025-10-17 13:15:44 +0800

    feat(ui): add LogoSplash

---
+++ src/components/ui/logo-splash.tsx
@@ -0,0 +1,9 @@
+import { Origami } from "lucide-react";
+
+export function LogoSplash() {
+  return (
+    <div className='flex h-dvh w-dvw items-center-safe justify-center-safe bg-black'>
+      {<Origami size={100} color='white' />}
+    </div>
+  );
+}
