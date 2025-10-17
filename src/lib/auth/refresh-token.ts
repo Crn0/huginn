90ee531fff66ca3709cba86cf644509f6d23c0c4
crno <118383162+Crn0@users.noreply.github.com.>
2025-10-17 12:48:32 +0800

    fix(auth/refresh-token): add API versioning to refresh token endpoint

--- src/lib/auth/refresh-token.ts
+++ src/lib/auth/refresh-token.ts
@@ -4,7 +4,9 @@ import { handleResponseError } from "../errors";
 import type { ErrorType } from "../errors/errors.type";
 
 export const refreshToken = async (signal?: AbortSignal): Promise<string> => {
-  const url = `${env.SERVER_URL}/auth/refresh-tokens`;
+  const API_VERSION = `v${env.API_VERSION}` as const;
+
+  const url = `${env.SERVER_URL}/api/${API_VERSION}/auth/refresh-tokens`;
 
   const res = await fetch(url, {
     signal,
