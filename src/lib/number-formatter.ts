a01ec80b766db6dc846ca534390af0f868690fb4
crno <118383162+Crn0@users.noreply.github.com.>
2025-10-22 15:20:12 +0800

    feat(lib): add nFormatter

---
+++ src/lib/number-formatter.ts
@@ -0,0 +1,18 @@
+export const nFormatter = (num: number, digit: number) => {
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "K" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" },
+  ];
+
+  const regexp = /\.0+$|(?<=\.[0-9]*[1-9])0+$/;
+  const item = lookup.findLast((item) => num >= item.value);
+
+  return item
+    ? (num / item.value).toFixed(digit).replace(regexp, "").concat(item.symbol)
+    : "0";
+};
