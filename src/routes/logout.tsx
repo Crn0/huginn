06d7f3f954fcce9069864fe2e8e314065141dd32
crno <118383162+Crn0@users.noreply.github.com.>
2025-10-22 15:22:56 +0800

    refactor(routes): move out logout route from protected route

---
+++ src/routes/logout.tsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+import {
+  Await,
+  createFileRoute,
+  useNavigate,
+  redirect,
+} from "@tanstack/react-router";
+import { useAuthActions } from "@/hooks/use-auth-store";
+import { LogoSplash } from "@/components/ui/logo-splash";
+
+const LogoutHandler = () => {
+  const { queryClient } = Route.useRouteContext();
+  const navigate = useNavigate();
+  const logout = useAuthActions().logout;
+
+  useEffect(() => {
+    queryClient.cancelQueries();
+    queryClient.clear();
+    logout();
+    navigate({ from: "/logout", to: "/login", replace: true });
+  }, [queryClient, logout, navigate]);
+
+  return <LogoSplash />;
+};
+
+export const Route = createFileRoute("/logout")({
+  beforeLoad: ({ context }) => {
+    if (!context.auth.isAuthenticated) {
+      throw redirect({ from: "/logout", to: "/login", replace: true });
+    }
+  },
+  loader: async ({ context: { client } }) => {
+    return {
+      p: client.callApi("auth/logout", { method: "POST" }),
+    };
+  },
+  component: RouteComponent,
+  errorComponent: LogoutHandler,
+});
+
+function RouteComponent() {
+  const { p } = Route.useLoaderData();
+
+  return (
+    <Await promise={p} fallback={<LogoSplash />}>
+      {() => <LogoutHandler />}
+    </Await>
+  );
+}
