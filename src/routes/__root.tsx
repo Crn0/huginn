a8070a0b63185fe13889348a52c12d8c9edd839d
crno <118383162+Crn0@users.noreply.github.com.>
2025-10-27 12:47:08 +0800

    feat(routes): add redirectTo search param

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,14 @@
+import type { QueryClient } from "@tanstack/react-query";
+import type { ApiClient } from "@/lib/api-client";
+import type { AuthStoreState } from "@/lib/provider";
+
 import * as React from "react";
+import z from "zod";
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { env } from "@/configs/env";
-
-import type { QueryClient } from "@tanstack/react-query";
-import type { ApiClient } from "@/lib/api-client";
-import type { AuthStoreState } from "@/lib/provider";
 
 interface RouterContext {
   client: ApiClient;
@@ -15,7 +16,31 @@ interface RouterContext {
   auth: AuthStoreState;
 }
 
+const paths = [
+  "/bookmarks",
+  "/compose",
+  "/explore",
+  "/home",
+  "/notifications",
+  "/settings",
+  "/compose/post",
+  "/settings/account",
+];
+
+const searchParamSchema = z.object({
+  redirectTo: z
+    .string()
+    .refine((p) => paths.includes(p) || /^\/.+$/.test(p), {
+      error: `Invalid redirect path. Allowed: ${[...paths].join(", ")}`,
+    })
+    .optional()
+    .catch("/home"),
+});
+
+export type RootSearchParam = z.infer<typeof searchParamSchema>;
+
 export const Route = createRootRouteWithContext<RouterContext>()({
+  validateSearch: (search) => searchParamSchema.parse(search),
   component: RootComponent,
 });
 
