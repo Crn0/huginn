c896668648fad70c29da8597322c4167f08a6259
crno <118383162+Crn0@users.noreply.github.com.>
2025-11-08 14:22:31 +0800

    build: add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,11 @@ export default defineConfig({
       target: "react",
       autoCodeSplitting: true,
     }),
-    react(),
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
     tailwindcss(),
   ],
   test: {
