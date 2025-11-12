0aa704118bfe173acaf17dff7df8a8fa124bce99
crno <118383162+Crn0@users.noreply.github.com.>
2025-11-12 14:39:39 +0800

    feat(utils): add formatDistance

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,7 +1,45 @@
-import { format, type DateArg, type FormatOptions } from "date-fns";
+import {
+  format,
+  formatDistanceStrict as originalFormatDistanceStrict,
+  type DateArg,
+  type FormatOptions,
+  type FormatDistanceStrictOptions,
+} from "date-fns";
+
+type D = (DateArg<Date> & {}) | string;
 
 export const formatDate = (
-  date: (DateArg<Date> & {}) | string,
+  date: D,
   formatStr: string,
   options?: FormatOptions
 ) => format(date, formatStr, options);
+
+export const formatDistance = (
+  laterDate: D,
+  earlierDate: D,
+  options?: FormatDistanceStrictOptions & { isUnitFirstChar: boolean }
+) => {
+  const distance = originalFormatDistanceStrict(
+    laterDate,
+    earlierDate,
+    options
+  );
+
+  if (!options?.isUnitFirstChar) {
+    return distance;
+  }
+
+  const time = distance
+    .split(" ")
+    .filter((_, i) => i <= 1)
+    .map((v, i) => (Number.isNaN(Number(v)) && i <= 1 ? v[0] : v))
+    .join("");
+
+  if (!options.addSuffix) {
+    return time;
+  }
+
+  const suffix = distance.split(" ")[2];
+
+  return `${time} ${suffix}`;
+};
