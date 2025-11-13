70c282a661ef998432166ed67e70fa77e94d19b7
crno <118383162+Crn0@users.noreply.github.com.>
2025-11-13 16:18:15 +0800

    feat(routes/compose): add replyTo search param

--- src/routes/_protected/compose.tsx
+++ src/routes/_protected/compose.tsx
@@ -1,6 +1,15 @@
+import z from "zod";
+
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
+const searchParamSchema = z.object({
+  replyTo: z.uuidv7().optional().catch(undefined),
+});
+
+export type ComposeSearchParam = z.infer<typeof searchParamSchema>;
+
 export const Route = createFileRoute("/_protected/compose")({
+  validateSearch: (search) => searchParamSchema.parse(search),
   component: RouteComponent,
 });
 
