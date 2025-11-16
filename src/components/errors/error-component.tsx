6e00c36f1d09a2ff8409f29a4760d26dafa89abb
crno <118383162+Crn0@users.noreply.github.com.>
2025-11-16 14:18:56 +0800

    refactor(error): simplify ErrorComponent and remove custom error handling logic

--- src/components/errors/error-component.tsx
+++ src/components/errors/error-component.tsx
@@ -1,46 +1,20 @@
-import { ErrorComponent as TanstackErrorComponent } from "@tanstack/react-router";
-
 import { Button } from "@/components/ui/button";
-
-import type { ErrorType } from "@/lib/errors/errors.type";
-
-const isCustomError = (e: Error | ErrorType): e is ErrorType =>
-  (e as ErrorType)?.kind !== undefined;
-
-const getErrorCode = (e: Error | ErrorType) => {
-  const defaultCode = 500;
-
-  if (isCustomError(e)) {
-    return e.code || defaultCode;
-  }
-
-  return defaultCode;
-};
 
 export function ErrorComponent({
   error,
-  reset,
+  reset = () => {},
 }: {
-  error: InstanceType<typeof Error>;
-  reset: () => void;
+  error: InstanceType<typeof Error> | null;
+  reset?: () => void;
 }) {
-  if (!isCustomError(error)) return <TanstackErrorComponent error={error} />;
-
   return (
-    <div role='alert'>
-      <p>Error {getErrorCode(error)}: Something went wrong</p>
-      <pre className='text-red-600'>{error.message}</pre>
-      {error &&
-        error.kind === "VALIDATION_ERROR" &&
-        error.issues?.length > 0 && (
-          <ul className='ml-4 list-disc'>
-            {error.issues.map((issue) => (
-              <li key={`${issue.path?.join(".")}:${issue.message}`}>
-                {issue.message}
-              </li>
-            ))}
-          </ul>
-        )}
+    <div
+      className='flex flex-col items-center-safe justify-center-safe gap-1'
+      role='alert'
+    >
+      <pre className='text-red-600'>
+        {error?.message ?? "Something went wrong."}
+      </pre>
       <Button type='button' onClick={() => reset()}>
         Try again
       </Button>
