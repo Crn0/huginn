a2fb450ff1019469d1ddd52d7283b3a354205e0e
crno <118383162+Crn0@users.noreply.github.com.>
2025-11-16 13:23:26 +0800

    feat: add infinite scroll hook and component

---
+++ src/components/ui/infinite-scroll.tsx
@@ -0,0 +1,38 @@
+import { Slot } from "@radix-ui/react-slot";
+
+import { cn } from "@/utils/cn";
+
+import {
+  useInfiniteScroll,
+  type UseInfiniteScrollOptions,
+} from "@/hooks/use-infinite-scroll";
+
+export interface InfiniteScrollProps
+  extends React.ComponentProps<"div">,
+    UseInfiniteScrollOptions {
+  testId?: string;
+  loadMore: () => void;
+  className?: string;
+  canLoadMore: boolean;
+  asChild?: boolean;
+}
+
+export function InfiniteScroll({
+  testId,
+  loadMore,
+  asChild,
+  className = "",
+  ...options
+}: InfiniteScrollProps) {
+  const Comp = asChild ? Slot : "div";
+
+  const setRef = useInfiniteScroll(loadMore, { ...options });
+
+  return (
+    <Comp
+      data-testid={testId}
+      ref={setRef}
+      className={cn("flex h-px justify-center-safe", className)}
+    />
+  );
+}
---
+++ src/hooks/use-infinite-scroll.ts
@@ -0,0 +1,60 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export interface UseInfiniteScrollOptions extends IntersectionObserverInit {
+  canLoadMore: boolean;
+  isLoading: boolean;
+  delay?: number;
+}
+
+export const useInfiniteScroll = (
+  loadMore: () => void,
+  options: UseInfiniteScrollOptions = {
+    isLoading: false,
+    canLoadMore: false,
+    root: null,
+    rootMargin: "0px",
+    threshold: 0,
+    delay: 0,
+  }
+) => {
+  const observer = useRef<IntersectionObserver | null>(null);
+  const timeoutId = useRef<NodeJS.Timeout | null>(null);
+  const [nodeRef, setNode] = useState<HTMLElement | null>(null);
+
+  const setNodeRef = useCallback((node: HTMLElement | null) => {
+    setNode(node);
+  }, []);
+
+  useEffect(() => {
+    if (options.isLoading || !options.canLoadMore || !nodeRef) return () => {};
+
+    const { root, rootMargin, threshold, delay } = options;
+
+    observer.current = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          if (delay) {
+            if (timeoutId.current) clearTimeout(timeoutId.current);
+
+            timeoutId.current = setTimeout(() => {
+              loadMore();
+            }, delay);
+          } else {
+            loadMore();
+          }
+        }
+      },
+      { root, rootMargin, threshold }
+    );
+
+    observer.current.observe(nodeRef);
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+  }, [loadMore, options, nodeRef]);
+
+  return setNodeRef;
+};
