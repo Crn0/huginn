a546041958d711654cb84baa17473745f91259af
crno <118383162+Crn0@users.noreply.github.com.>
2025-11-16 14:12:34 +0800

    feat(routes/home): implement tabbed home tweets page with user-scoped filtering

--- src/routes/_protected/home.tsx
+++ src/routes/_protected/home.tsx
@@ -1,9 +1,45 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { useAuthUser } from "@/lib/auth";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tweets } from "@/features/tweets/components/tweets";
+import { ContentLayout } from "@/components/layouts/content-layout";
+
 export const Route = createFileRoute("/_protected/home")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/_protected/home"!</div>;
+  const userQuery = useAuthUser();
+
+  if (!userQuery.isSuccess) return null;
+
+  const user = userQuery.data;
+
+  return (
+    <ContentLayout>
+      <Tabs defaultValue='all'>
+        <TabsList className='sticky top-10 z-30 w-full sm:top-0'>
+          <TabsTrigger value='all'>
+            <span className='group-data-[state=active]:border-b-5 group-data-[state=active]:border-b-blue-400'>
+              For you
+            </span>
+          </TabsTrigger>
+          <TabsTrigger value='following'>
+            <span className='group-data-[state=active]:border-b-5 group-data-[state=active]:border-b-blue-400'>
+              Following
+            </span>
+          </TabsTrigger>
+        </TabsList>
+
+        <TabsContent value='all'>
+          <Tweets username={user.username} filter={{ scope: "all" }} />
+        </TabsContent>
+
+        <TabsContent value='following'>
+          <Tweets username={user.username} filter={{ scope: "following" }} />
+        </TabsContent>
+      </Tabs>
+    </ContentLayout>
+  );
 }
