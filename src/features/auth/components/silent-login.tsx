1da7581b98b8f068a99ce3a03563f97d4bb77e45
crno <118383162+Crn0@users.noreply.github.com.>
2025-11-22 15:52:48 +0800

    feat(auth): add SilentLogin

---
+++ src/features/auth/components/silent-login.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useNavigate, useSearch } from "@tanstack/react-router";
+
+import { useAuthActions } from "@/hooks/use-auth-store";
+
+import { LogoSplash } from "@/components/ui/logo-splash";
+
+export function SilentLogin({
+  children,
+  token,
+}: {
+  children: React.ReactNode;
+  token: string | null;
+}) {
+  const navigate = useNavigate();
+  const search = useSearch({ from: "__root__" });
+
+  const redirectTo = search.redirectTo ?? "/home";
+  const login = useAuthActions().login;
+  const setIsSilentLogin = useAuthActions().setIsSilentLogin;
+
+  useEffect(() => {
+    if (token) {
+      login(token);
+
+      navigate({ to: redirectTo, replace: true });
+    } else {
+      setIsSilentLogin();
+
+      navigate({ to: "/login", replace: true });
+    }
+  }, [token, login, navigate, redirectTo, setIsSilentLogin]);
+
+  if (token) return <LogoSplash />;
+
+  return children;
+}
