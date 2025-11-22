01e22a0e253fef8f3364145e23f68e993396d13f
crno <118383162+Crn0@users.noreply.github.com.>
2025-11-22 12:44:28 +0800

    refactor(layout): add contentClassName and headerClassName props and remove border

--- src/components/layouts/content-layout.tsx
+++ src/components/layouts/content-layout.tsx
@@ -1,22 +1,37 @@
+import { cn } from "@/utils/cn";
 import type { PropsWithChildren, ReactNode } from "react";
 
 export type ContentLayoutProps = PropsWithChildren & {
   headerChildren?: ReactNode;
+  headerClassName?: string;
+  contentClassName?: string;
 };
 
 export function ContentLayout({
   children,
   headerChildren,
+  headerClassName,
+  contentClassName,
 }: ContentLayoutProps) {
   return (
     <>
       {headerChildren && (
-        <header className='bg-background border-border sticky top-0 z-30 flex flex-1 items-center-safe justify-between border-l'>
+        <header
+          className={cn(
+            "bg-background sticky top-0 z-30 flex flex-1 items-center-safe justify-between",
+            headerClassName
+          )}
+        >
           {headerChildren}
         </header>
       )}
 
-      <div className='border-border flex h-full w-full flex-col border-r border-l bg-inherit'>
+      <div
+        className={cn(
+          "flex h-full w-full flex-col border-r bg-inherit",
+          contentClassName
+        )}
+      >
         {children}
       </div>
     </>
