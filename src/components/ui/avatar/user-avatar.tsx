2f104a657d71703e1f06e04cb1c99dd1fd95c6ad
crno <118383162+Crn0@users.noreply.github.com.>
2025-12-01 17:08:35 +0800

    refactor(user-avatar): add default fallback string

--- src/components/ui/avatar/user-avatar.tsx
+++ src/components/ui/avatar/user-avatar.tsx
@@ -18,7 +18,7 @@ export function UserAvatar({
     <Avatar className={cn("text-foreground border-5 border-black", className)}>
       {avatar && <AvatarImage src={avatar} />}
       <AvatarFallback className={cn("bg-teal-600", fallbackClassName)}>
-        {fallback[0].toUpperCase()}
+        {fallback[0]?.toUpperCase() ?? "H"}
       </AvatarFallback>
     </Avatar>
   );
