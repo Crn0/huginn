ce398c9f4b9bbe88879ec891e80117a8d9000dd4
crno <118383162+Crn0@users.noreply.github.com.>
2025-12-27 13:16:06 +0800

    refactor(try-catch): enhance error handling and simplify function signatures

--- src/lib/try-catch.ts
+++ src/lib/try-catch.ts
@@ -1,62 +1,62 @@
-import type { ErrorType } from "./errors/errors.type";
-
 import { debugError } from "./logger";
 
 export type OperationSuccess<T> = { error: null; data: T };
 export type OperationFailure<E> = { error: E; data: null };
 export type OperationResult<T, E> = OperationSuccess<T> | OperationFailure<E>;
 
-type Operation<T> = Promise<T> | (() => Promise<T>) | (() => T);
-type OnFinally = (() => void) | (() => Promise<void>);
-type TransformError<E extends ErrorType> = (message: string) => E;
-type Options<E extends ErrorType> = {
-  transformError?: TransformError<E>;
-  onFinally?: OnFinally;
+type Identity<T> = {
+  [Key in keyof T]: T[Key];
 };
 
-export function tryCatch<T, E extends ErrorType>(
-  operation: Promise<T>,
-  options?: Options<E>
-): Promise<OperationResult<T, E>>;
-export function tryCatch<T, E extends ErrorType>(
-  operation: T,
-  options?: Options<E>
-): Promise<OperationResult<T, E>>;
-export function tryCatch<T, E extends ErrorType>(
-  operation: () => Promise<T>,
-  options?: Options<E>
-): Promise<OperationResult<T, E>>;
-export function tryCatch<T, E extends ErrorType>(
-  operation: Operation<T>,
-  options?: Options<E>
-): OperationResult<T, E> | Promise<OperationResult<T, E>> {
-  const opts = typeof options === "object" && options ? options : {};
+type Operation<T> = Promise<T> | (() => Promise<T>) | (() => T);
+type TransformError<TError> = (options: Identity<TError>) => TError;
 
-  const transformError = opts?.transformError;
-  const onFinally = opts?.onFinally;
-
+export function tryCatch<TResult, TError extends Error & { status?: number }>(
+  operation: () => TResult,
+  transformError?: TransformError<TError>
+): OperationResult<TResult, TError>;
+export function tryCatch<TResult, TError extends Error & { status?: number }>(
+  operation: Promise<TResult>,
+  transformError?: TransformError<TError>
+): Promise<OperationResult<TResult, TError>>;
+export function tryCatch<TResult, TError extends Error & { status?: number }>(
+  operation: TResult,
+  transformError?: TransformError<TError>
+): Promise<OperationResult<TResult, TError>>;
+export function tryCatch<TResult, TError extends Error & { status?: number }>(
+  operation: () => Promise<TResult>,
+  transformError?: TransformError<TError>
+): Promise<OperationResult<TResult, TError>>;
+export function tryCatch<TResult, TError extends Error & { status?: number }>(
+  operation: Operation<TResult>,
+  transformError?: TransformError<TError>
+):
+  | OperationResult<TResult, TError>
+  | Promise<OperationResult<TResult, TError>> {
   try {
     const result = typeof operation === "function" ? operation() : operation;
 
     if (isPromise(result)) {
       return Promise.resolve(result)
         .then((data) => onSuccess(data))
-        .catch((error) => onFailure(error));
+        .catch((error) => {
+          const err =
+            typeof transformError === "function"
+              ? transformError(error as TError)
+              : error;
+
+          return onFailure<TError>(err);
+        });
     }
 
     return onSuccess(result);
-  } catch (e) {
-    debugError(e);
+  } catch (error) {
+    const err =
+      typeof transformError === "function"
+        ? transformError(error as TError)
+        : error;
 
-    if (typeof transformError === "function") {
-      return { error: transformError(String(e)), data: null };
-    }
-
-    return onFailure<E>(e);
-  } finally {
-    const cleanUp = typeof onFinally === "function" ? onFinally() : onFinally;
-
-    Promise.resolve(cleanUp).then().catch(debugError);
+    return onFailure<TError>(err);
   }
 }
 
@@ -64,10 +64,12 @@ const onSuccess = <T>(data: T): OperationSuccess<T> => {
   return { data, error: null };
 };
 
-const onFailure = <E>(error: unknown): OperationFailure<E> => {
+const onFailure = <E extends Error>(error: unknown): OperationFailure<E> => {
   const message = String(error) || "Something went wrong";
 
   const errorParsed = error instanceof Error ? error : new Error(message);
+
+  debugError(errorParsed);
 
   return { error: errorParsed as E, data: null };
 };
