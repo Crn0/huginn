e2f76eeb5981bcde02665566a0450d2aeab19544
crno <118383162+Crn0@users.noreply.github.com.>
2025-12-29 16:33:45 +0800

    style(logo-splash): set width to 100 percent

--- src/components/ui/logo-splash.tsx
+++ src/components/ui/logo-splash.tsx
@@ -2,7 +2,7 @@ import { Origami } from "lucide-react";
 
 export function LogoSplash() {
   return (
-    <div className='flex h-dvh w-dvw items-center-safe justify-center-safe bg-black'>
+    <div className='flex h-dvh w-full items-center-safe justify-center-safe bg-black'>
       {<Origami size={100} color='white' />}
     </div>
   );
